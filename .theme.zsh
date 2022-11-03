340da9c5dfb308b6568bce97b76611fc56f4adb5
Raccie <43726319+Raccie@users.noreply.github.com>
2022-11-03 11:47:58 +0100

    correctly escape colors
    this is done because otherwise tab completion glitches out and puts the prompt several spaces to the right

--- .theme.zsh
+++ .theme.zsh
@@ -1,6 +1,9 @@
 autoload -Uz vcs_info
 autoload -Uz colors && colors
 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
@@ -10,12 +13,11 @@ function check_last_exit_code() {
   local LAST_EXIT_CODE=$?
   if [[ $LAST_EXIT_CODE -ne 0 ]]; then
     local EXIT_CODE_PROMPT=''
-    EXIT_CODE_PROMPT+="$fg[red][$fg_bold[red]$LAST_EXIT_CODE$fg_no_bold[red]]$reset_color"
+    EXIT_CODE_PROMPT+="%{$fg[red]%}[%{$fg_bold[red]%}$LAST_EXIT_CODE%{$fg_no_bold[red]%}]%{$reset_color%}"
     echo "$EXIT_CODE_PROMPT"
   fi
 }
 
-PROMPT=''
-PROMPT+='$fg[magenta]%n$reset_color@%m '
-PROMPT+='$fg[magenta]%~$reset_color'
-PROMPT+='${vcs_info_msg_0_} $(check_last_exit_code)$ '
+PROMPT='%{$fg[magenta]%}%n%{$reset_color%}@%m '
+PROMPT+='%{$fg[magenta]%}%~%{$reset_color%}${vcs_info_msg_0_} '
+PROMPT+='$(check_last_exit_code)$ '
